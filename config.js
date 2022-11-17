5fbe1924caca3dd49ef94774fb47fd58fcf86091
unknown <1538851062@qq.com>
2022-11-17 20:26:44 +0800

    update

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const config = {
     nav: [
         { name: author, link: "/", children: [], liClass: 'title', rel: 'start', iClass: '', aClass: '' },
         { name: '首页', link: "/", children: [], liClass: '', rel: 'section', iClass: 'i-feather', aClass: '' },
-        { name: '归档', link: "/archives", children: [], liClass: '', rel: 'section', iClass: 'i-list-alt', aClass: '' },
-        { name: '标签', link: "/tags", children: [], liClass: '', rel: 'section', iClass: 'i-tags', aClass: '' },
-        { name: '收藏', link: "/WebSites", children: [], liClass: '', rel: 'section', iClass: 'i-star', aClass: '' },
+        { name: '归档', link: "/archive", children: [], liClass: '', rel: 'section', iClass: 'i-list-alt', aClass: '' },
+        { name: '标签', link: "/tag", children: [], liClass: '', rel: 'section', iClass: 'i-tag', aClass: '' },
+        { name: '收藏', link: "/site", children: [], liClass: '', rel: 'section', iClass: 'i-star', aClass: '' },
         { name: '关于', link: "/about", children: [], liClass: '', rel: 'section', iClass: 'i-cloud', aClass: '' },
         { name: 'Github', link: "#", children: [], liClass: '', rel: 'noopener external nofollow noreferrer', iClass: 'i-paper-plane', aClass: '' }
     ],
@@ -22,27 +22,23 @@ const config = {
         tagsNum: '33',
         socialLinks: [
             { url: 'https://github.com/creacing/Close_To_Me', name: 'github', icon: "i-github" },
-            // { url: 'https://twitter.com/', name: 'twitter', icon: "i-twitter" },
-            // { url: 'https://www.zhihu.com/people/', name: 'zhihu', icon: "i-zhihu" },
-            // { url: 'https://music.163.com/', name: 'music', icon: "i-cloud-music" },
-            // { url: 'https://t.me/', name: 'telegram', icon: "i-paper-plane" },
         ],
         localPath: [
             { url: '/', icon: 'i-feather', name: 'could1', },
-            { url: '/archives', icon: 'i-list-alt', name: 'could2', },
-            { url: '/tags', icon: 'i-tags', name: 'could3', },
-            { url: '/WebSites', icon: 'i-star', name: 'could4', },
+            { url: '/archive', icon: 'i-list-alt', name: 'could2', },
+            { url: '/tag', icon: 'i-tag', name: 'could3', },
+            { url: '/site', icon: 'i-star', name: 'could4', },
             { url: '/about', icon: 'i-cloud', name: 'could5', }
         ]
     },
     sites: [
-        { url: "https://www.remove.bg/zh", title: "在线抠图", color: "", description: '在线抠图' },
+        { url: "https://www.remove.bg/zh", title: "抠图", color: "", description: '在线抠图' },
         { url: "http://www.sortablejs.com/", title: "sortablejs", color: "", description: '功能强大的javascript拖拽库' },
-        { url: "http://tool.c7sky.com/webcolor/", title: "网页设计常用色彩搭配表", color: "", description: '网页设计常用色彩搭配表' },
-        { url: "http://www.googlefonts.net/", title: "谷歌字体", color: "", description: '谷歌字体' },
-        { url: "https://jpgmin.cn/", title: "压图大师", color: "", description: '压图大师' },
-        { url: "https://www.fontke.com/tool/rgb/eee5f8/", title: "颜色查询", color: "", description: '颜色查询' },
-        { url: "https://animista.net/play/basic/rotate", title: "css动画大全", color: "", description: 'css动画大全' }
+        { url: "http://tool.c7sky.com/webcolor/", title: "色彩搭配表", color: "", description: '网页设计常用的色彩搭配表' },
+        { url: "http://www.googlefonts.net/", title: "字体", color: "", description: '谷歌字体' },
+        { url: "https://jpgmin.cn/", title: "压缩图片", color: "", description: '压图大师' },
+        { url: "https://www.fontke.com/tool/rgb/eee5f8/", title: "查询颜色", color: "", description: '颜色查询' },
+        { url: "https://animista.net/play/basic/rotate", title: "css动画", color: "", description: 'css动画大全' }
 
         
     ],
